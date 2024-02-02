983ccc735f0e1e08080f9303eab70c75783f2f03
Vini Bjazevich <vinibjazevich97@gmail.com>
2024-02-02 00:23:47 -0800

    feat: created matching header for ios and andriod

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from "react-native";
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text>Open up App.js to start working app!</Text>
+      </View>
+      <ExpoStatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: isAndroid ? StatusBar.currentHeight : 0,
+    backgroundColor: "red",
+  },
+  header: {
+    height: 123,
+    backgroundColor: "green",
   },
 });
---
+++ README.md
@@ -0,0 +1,8 @@
+To run your project, run one of the following npm commands.
+
+- npm run android
+- npm run ios
+- npm run web
+
+- Run eas build when you are ready to create your first build.
+- Once the build is completed, run eas submit to upload the app to app stores.
