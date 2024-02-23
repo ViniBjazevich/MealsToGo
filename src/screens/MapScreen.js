6123df1cab18d17dd714ff43e8be6218232bd335
Vini Bjazevich <vinibjazevich97@gmail.com>
2024-02-23 15:08:54 -0800

    feat: added react native maps

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,9 +1,20 @@
 import { View, Text } from "react-native";
+import MapView from "react-native-maps";
+import styled from "styled-components/native";
+
+const MapContainer = styled.View`
+  flex: 1;
+`;
+
+const Map = styled(MapView)`
+  width: 100%;
+  height: 100%;
+`;
 
 export const MapScreen = () => {
   return (
-    <View>
-      <Text>Map Screen</Text>
-    </View>
+    <MapContainer>
+      <Map />
+    </MapContainer>
   );
 };
